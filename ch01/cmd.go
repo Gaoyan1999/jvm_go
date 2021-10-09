d6b4da4d1fb96ba3be601984fc08c084f9b5f36e
danielgao <danielgao@yaocheng.cn>
2021-10-09 23:31:50 +0800

    aa

---
+++ ch01/cmd.go
@@ -0,0 +1,36 @@
+package main
+
+import "flag"
+import "fmt"
+import "os"
+
+type Cmd struct {
+	helpFlag bool
+	versionFlag bool
+	cpOption string
+	class string
+	args []string
+}
+
+
+func parseCmd() *Cmd {
+	cmd := &Cmd{}
+	// flag 可以帮助我们处理命令行选项。
+	flag.Usage = printUsage
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+	return cmd
+}
+func printUsage() {
+	// os包定义了一个Args变量，其中存放传递给命令行的全部参数
+	fmt.Printf("Usage: %s [-options] class [args...]\n", os.Args[0])
+}
---
+++ ch02/classpath/entry.go
@@ -0,0 +1,31 @@
+package classpath
+
+import (
+	"os"
+	"strings"
+)
+
+// 存放分隔符
+const pathListSeparator = string(os.PathListSeparator)
+
+type Entry interface {
+	// 寻找和加载class文件
+	readClass(className string)([]byte,Entry,error)
+	// String（）方法的作用相当于Java中的toString（）
+	String() string
+}
+
+// 根据 path 解析不同的 entry
+func newEntry(path string) Entry {
+	if strings.Contains(path,pathListSeparator) {
+		return newCompositeEntry(path)
+	}
+	if strings.HasSuffix(path,"*") {
+		// java -cp classes;lib\* ...
+		return newWildcardEntry(path)
+	}
+	if strings.HasSuffix(path,".jar") || strings.HasSuffix(path,".JAR") || strings.HasSuffix(path,".zip") || strings.HasSuffix(path,".ZIP") {
+		return newZipEntry(path)
+	}
+	return newDirEntry(path)
+}
