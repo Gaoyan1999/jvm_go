514fea5694983d7f790bd2f55a6251fc9e0378d0
danielgao <1803483451@qq.com>
2022-02-22 01:02:24 +0800

    fix interpreter

--- ch05/instructions/factory.go
+++ ch05/instructions/factory.go
@@ -7,14 +7,38 @@ import (
 import . "jvmgo/ch05/instructions/const"
 
 var (
-	nop = &NOP{}
+	nop         = &NOP{}
+	aconst_null = &ACONST_NULL{}
+	iconst_m1   = &ICONST_M1{}
+	iconst_0    = &ICONST_0{}
+	iconst_1    = &ICONST_1{}
+	iconst_2    = &ICONST_2{}
+	iconst_3    = &ICONST_3{}
+	iconst_4    = &ICONST_4{}
+	iconst_5    = &ICONST_5{}
 )
 
 func NewInstruction(opcode byte) base.Instruction {
 
 	switch opcode {
-	case 0x00: return &NOP{}
-	case 0x01: return &ACONST_NULL{}
+	case 0x00:
+		return nop
+	case 0x01:
+		return aconst_null
+	case 0x02:
+		return iconst_m1
+	case 0x03:
+		return iconst_0
+	case 0x04:
+		return iconst_1
+	case 0x05:
+		return iconst_2
+	case 0x06:
+		return iconst_3
+	case 0x07:
+		return iconst_4
+	case 0x08:
+		return iconst_5
 
 	default:
 		panic(fmt.Errorf("Unspported opcode: 0x%x!", opcode))
