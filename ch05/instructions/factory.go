fe23e296d0b515998e76d752670537b352926826
danielgao <1803483451@qq.com>
2022-03-05 11:43:44 +0800

    add fstore and istore

--- ch05/instructions/factory.go
+++ ch05/instructions/factory.go
@@ -3,10 +3,10 @@ package instructions
 import (
 	"fmt"
 	"jvmgo/ch05/instructions/base"
-
 )
 import . "jvmgo/ch05/instructions/const"
 import . "jvmgo/ch05/instructions/load"
+import . "jvmgo/ch05/instructions/store"
 
 var (
 	nop         = &NOP{}
@@ -25,17 +25,27 @@ var (
 	fconst_2    = &FCONST_2{}
 	dconst_0    = &DCONST_0{}
 	dconst_1    = &DCONST_1{}
-	iload_0 	= &ILOAD_0{}
-	iload_1 	= &ILOAD_1{}
-	iload_2 	= &ILOAD_2{}
-	iload_3		= &ILOAD_3{}
-	lload_0		= &LLoad_0{}
-	lload_1		= &LLoad_1{}
-	lload_2		= &LLoad_2{}
-	lload_3		= &LLoad_3{}
-
-
-
+	iload_0     = &ILOAD_0{}
+	iload_1     = &ILOAD_1{}
+	iload_2     = &ILOAD_2{}
+	iload_3     = &ILOAD_3{}
+	lload_0     = &LLoad_0{}
+	lload_1     = &LLoad_1{}
+	lload_2     = &LLoad_2{}
+	lload_3     = &LLoad_3{}
+	// TODO: fLoad dLoad aLoad
+	istore_0 = &ISTORE_0{}
+	istore_1 = &ISTORE_1{}
+	istore_2 = &ISTORE_2{}
+	istore_3 = &ISTORE_3{}
+	lstore_0 = &LSTORE_0{}
+	lstore_1 = &LSTORE_1{}
+	lstore_2 = &LSTORE_2{}
+	lstore_3 = &LSTORE_3{}
+	fstore_0 = &FSTORE_0{}
+	fstore_1 = &FSTORE_1{}
+	fstore_2 = &FSTORE_2{}
+	fstore_3 = &FSTORE_3{}
 )
 
 func NewInstruction(opcode byte) base.Instruction {
@@ -94,7 +104,31 @@ func NewInstruction(opcode byte) base.Instruction {
 	case 0x21:
 		return lload_3
 
-
+	// TODO:
+	case 0x3b:
+		return istore_0
+	case 0x3c:
+		return istore_1
+	case 0x3d:
+		return istore_2
+	case 0x3e:
+		return istore_3
+	case 0x3f:
+		return lstore_0
+	case 0x40:
+		return lstore_1
+	case 0x41:
+		return lstore_2
+	case 0x42:
+		return lstore_3
+	case 0x43:
+		return fstore_0
+	case 0x44:
+		return fstore_1
+	case 0x45:
+		return fstore_2
+	case 0x46:
+		return fstore_3
 
 	default:
 		panic(fmt.Errorf("Unspported opcode: 0x%x!", opcode))
---
+++ ch05/instructions/store/fstore.go
@@ -0,0 +1,42 @@
+package store
+
+import (
+	"jvmgo/ch05/instructions/base"
+	"jvmgo/ch05/rtda"
+)
+
+// Store float into local variable
+type FSTORE struct{ base.Index8Instruction }
+
+func (ins *FSTORE) Execute(frame *rtda.Frame) {
+	_fstore(frame, ins.Index)
+}
+
+type FSTORE_0 struct{ base.NoOperandsInstruction }
+
+func (ins *FSTORE_0) Execute(frame *rtda.Frame) {
+	_fstore(frame, 0)
+}
+
+type FSTORE_1 struct{ base.NoOperandsInstruction }
+
+func (ins *FSTORE_1) Execute(frame *rtda.Frame) {
+	_fstore(frame, 1)
+}
+
+type FSTORE_2 struct{ base.NoOperandsInstruction }
+
+func (ins *FSTORE_2) Execute(frame *rtda.Frame) {
+	_fstore(frame, 2)
+}
+
+type FSTORE_3 struct{ base.NoOperandsInstruction }
+
+func (ins *FSTORE_3) Execute(frame *rtda.Frame) {
+	_fstore(frame, 3)
+}
+
+func _fstore(frame *rtda.Frame, index uint) {
+	val := frame.OperandStack.PopFloat()
+	frame.LocalVars.SetFloat(index, val)
+}
